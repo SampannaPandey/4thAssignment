25d84f644e2f1f24c8b47fbae8a41a650b957216
Sampanna Pandey <pandeysam7@gmail.com>
2019-05-31 16:11:53 +0545

    Initial commit

---
+++ OnlineClothingAPI/app/src/main/java/model/LoginSignupResponse.java
@@ -0,0 +1,21 @@
+package model;
+
+public class LoginSignupResponse {
+    private boolean success;
+    private String status;
+    private String token;
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+}
